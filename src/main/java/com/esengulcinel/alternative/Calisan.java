eaec9388da6f3c073693a191bd171c1ff4584205
EsengulCinelJForce <esengul.cinel@jforce.com>
2022-09-08 00:41:33 +0300

    Alvernative

---
+++ src/main/java/com/esengulcinel/alternative/Calisan.java
@@ -0,0 +1,25 @@
+package com.esengulcinel.alternative;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named(value = "alternativedi")
+@ApplicationScoped
+
+public class Calisan {
+
+    @Inject
+    private PatronInterface patronInterface;
+
+//    //parametresiz cons
+//    Calisan() {
+//
+//      //  patronInterface = new Patron();
+//    }
+
+    public String getData(String data) {
+
+        return patronInterface.surum(data);
+    }
+}
---
+++ src/main/java/com/esengulcinel/alternative/PatronInterface.java
@@ -0,0 +1,5 @@
+package com.esengulcinel.alternative;
+
+public interface PatronInterface {
+    public String surum(String data);
+}
