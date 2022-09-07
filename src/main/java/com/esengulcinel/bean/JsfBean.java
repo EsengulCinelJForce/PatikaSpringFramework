c11a8e3f14f7f21f1c440ae1fecfc208fe24e1be
EsengulCinelJForce <esengul.cinel@jforce.com>
2022-09-07 14:03:52 +0300

    JavaTutorialsIFNeeded

--- src/main/java/com/esengulcinel/bean/JsfBean.java
+++ src/main/java/com/esengulcinel/bean/JsfBean.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 
+
 @Named(value = "cdiBean")
 @RequestScoped
 @Getter @Setter
---
+++ src/main/java/com/esengulcinel/tutorials/JavaTutorials.java
@@ -0,0 +1,30 @@
+package com.esengulcinel.tutorials;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+
+        //TEKRAR EDİLEBİLECEK KONULAR
+
+        // Arrays.sort(); --dizilere
+        //this  --global const bileşenlerini çağırmak için kullanıcaz
+        //super --kalıtım ile gelen veriyi erişmek
+        //break contunie return -- yapıları
+        //optional yapıları
+        // cast -- int to string , string to int , boxing un boxing
+        //primitive type
+        //wrapper class
+        //Random ve math
+        //final -- değişkinler sabitleyici, metotlarda override edilemez, classlarda kullanırsam kalıtım yapılamaz
+        //static
+        //decoder , encoder
+        //instanceof -- bir nesnenin o sınıfa ait olup olmadığı yapıları
+        //access idendify --public private protected
+        //Enum multithread
+
+        //Solid single responsiblity -- tek görev bir kod sadece bir işlem yapsın prensibleri
+        //DRY --dont repeat yourself
+        //MONAD -- bir metodun çıktısı başka bir metodun girdisi anlamına gelir
+        //List set map
+        //
+    }
+}
